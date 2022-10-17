e1f94c11faae2baf42dbfc2e96084884ff6e73c6
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-10-17 10:12:28 +0500

    react-router-dom error fixed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import './assets/style/global.scss'
 import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import MarkDown from "./pages/Blog/Blogs"
+import MarkDown from "./pages/Blog/Blogs";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/new-portfolio">
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} ></Route>
         {/* <Route path='/blog' element={<Blog />} /> */}
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/markdown/:id/:itemId' element={<MarkDown />} />
+        <Route path='/contact' element={<Contact />} ></Route>
+        {/* <Route path='/markdown/:id/:itemId' element={<MarkDown />} /> */}
       </Routes>
     </BrowserRouter>
   );
